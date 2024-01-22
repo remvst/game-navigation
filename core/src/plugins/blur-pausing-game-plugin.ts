1486d61138dc187f2dfd30a9850fdb4b1cf34c8f
remvst <remvst@gmail.com>
2024-01-22 01:23:37 -0500

    Added enabled flag on BlurPausingGamePlugin

--- core/src/plugins/blur-pausing-game-plugin.ts
+++ core/src/plugins/blur-pausing-game-plugin.ts
@@ -5,15 +5,26 @@ export class BlurPausingGamePlugin extends GamePlugin {
     static readonly key = 'blur-pausing-game';
     readonly key = BlurPausingGamePlugin.key;
 
+    private _enabled = false;
+
+    get enabled() {
+        return this._enabled;
+    }
+
+    set enabled(enabled: boolean) {
+        this._enabled = enabled;
+        this.updateLoop();
+    }
+
     setup(): void {
         super.setup();
 
-        window.addEventListener('blur', () => this.focusChanged(), false);
-        window.addEventListener('focus', () => this.focusChanged(), false);
+        window.addEventListener('blur', () => this.updateLoop(), false);
+        window.addEventListener('focus', () => this.updateLoop(), false);
     }
 
-    private focusChanged() {
-        if (document.hasFocus()) {
+    private updateLoop() {
+        if (document.hasFocus() || !this._enabled) {
             this.game.scheduleFrame();
         } else {
             this.game.stopLoop();

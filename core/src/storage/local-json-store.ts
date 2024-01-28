4f186f56f626471fa15e8b4fc5ae3394d70543f6
remvst <remvst@gmail.com>
2024-01-28 14:08:24 -0500

    item() API

--- core/src/storage/local-json-store.ts
+++ core/src/storage/local-json-store.ts
@@ -1,7 +1,32 @@
+export class StoredItem<T> {
+    constructor(
+        private readonly localJsonStore: LocalJsonStore,
+        readonly key: string,
+    ) {
+
+    }
+
+    get(): Promise<T | null> {
+        return this.localJsonStore.getItem(this.key);
+    }
+
+    set(value: T): Promise<void> {
+        return this.localJsonStore.setItem(this.key, value);
+    }
+
+    delete(): Promise<void> {
+        return this.localJsonStore.deleteItem(this.key);
+    }
+}
+
 export class LocalJsonStore {
     constructor(
         private readonly localStorage: Storage,
     ) {
+    }
+
+    item<T>(key: string): StoredItem<T> {
+        return new StoredItem(this, key);
     }
 
     async setItem(key: string, value: any): Promise<void> {
@@ -24,7 +49,9 @@ export class LocalJsonStore {
 
     async getItem(key: string): Promise<any> {
         try {
-            return JSON.parse(this.localStorage.getItem(key));
+            const stored = this.localStorage.getItem(key);
+            if (!stored) return null;
+            return JSON.parse(stored);
         } catch (err) {
             console.error(`getItem error: ${err.message}. Falling back to null`, err)
             return null;

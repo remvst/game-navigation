8b2cc2d079ca962767e18ed20a58a621e649ec36
remvst <remvst@gmail.com>
2024-04-03 02:18:12 -0400

    Improve navigationFlow() API

--- core/src/util/navigation-flow.ts
+++ core/src/util/navigation-flow.ts
@@ -1,8 +1,22 @@
 import { UserCancelledError } from "../navigation/resolver";
 
-export function navigationFlow<T>(flow: Promise<T>) {
-    flow.catch((err) => {
+export function navigationFlow<T>(flow: Promise<T> | (() => Promise<T>)): void {
+    let promise: Promise<T>;
+
+    if (flow instanceof Function) {
+        promise = flow();
+    }
+
+    if (flow instanceof Promise) {
+        promise = flow;
+    }
+
+    promise.catch((err) => {
         if (err instanceof UserCancelledError) return;
         console.error(err);
     });
+}
+
+export function navigationClickHandler<T>(flow: (() => Promise<T>)): () => void {
+    return () => navigationFlow(flow);
 }

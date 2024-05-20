2562ecbb34b24874375915c9928da63e8db65183
remvst <remvst@gmail.com>
2024-05-20 16:11:51 -0400

    Don't hide react container when backgrounded

--- react/src/react-screen.ts
+++ react/src/react-screen.ts
@@ -17,29 +17,25 @@ export abstract class ReactScreen extends HTMLScreen {
 
         this.rootContainer.style.width = "100%";
         this.rootContainer.style.height = "100%";
-    }
 
-    foreground(): void {
-        super.foreground();
-        this.view.style.display = "block";
         this.view.appendChild(this.rootContainer);
-    }
-
-    background(): void {
-        super.background();
-        this.view.style.display = "none";
-        this.rootContainer.parentNode?.removeChild(this.rootContainer);
     }
 
     destroy() {
         super.destroy();
-        this.root.unmount();
+        this.root?.unmount();
         this.root = null;
     }
 
     rebuild() {
-        this.root.unmount();
-        this.root = createRoot(this.rootContainer);
-        this.rootComponent().then((component) => this.root.render(component));
+        this.root?.unmount();
+
+        const root = createRoot(this.rootContainer);
+        this.root = root;
+
+        this.rootComponent().then((component) => {
+            if (this.root !== root) return;
+            this.root?.render(component);
+        });
     }
 }

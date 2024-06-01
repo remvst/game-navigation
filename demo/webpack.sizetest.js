589fff49dc6d9d0f4ee8a0c6b817208103ad21c9
Remi Vansteelandt <remvst@gmail.com>
2024-06-01 00:31:05 -0400

    Added sizetest to demo

---
+++ demo/webpack.sizetest.js
@@ -0,0 +1,13 @@
+const { merge } = require("webpack-merge");
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+    mode: "production",
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+        }),
+    ],
+});

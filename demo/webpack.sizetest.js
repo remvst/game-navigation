2139a316b5fe4fa2e6f664e230f8d0a9848e0df4
Remi Vansteelandt <remvst@gmail.com>
2024-06-04 14:23:40 -0400

    kfine

--- demo/webpack.sizetest.js
+++ demo/webpack.sizetest.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 const common = require("./webpack.common.js");
 
@@ -7,7 +7,7 @@ module.exports = merge(common, {
     mode: "production",
     plugins: [
         new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
+            analyzerMode: "static",
         }),
     ],
 });
